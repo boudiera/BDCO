1a0942ccfbecac301cee22bd73a55610fa152e95
Florian Mourin <florian.mourin@phelma.grenoble-inp.fr>
2016-04-13 15:34:43 +0200

    classes modele avec getter et setter

--- RestaurantBDCO/src/Modele/Reservation.java
+++ RestaurantBDCO/src/Modele/Reservation.java
@@ -9,11 +9,12 @@ import java.sql.*;
 public abstract class Reservation {
     private ArrayList<Integer> codeTable;
     private int nbPersonnes;
-    private int nomClient;
+    private String nomClient;
     private Date jour;
     private float heure;
+    private int tel;
 
-    public Reservation(ArrayList<Integer> codeTable, int nbPersonnes, int nomClient, Date jour, float heure) {
+    public Reservation(ArrayList<Integer> codeTable, int nbPersonnes, String nomClient, Date jour, float heure) {
         this.codeTable = codeTable;
         this.nbPersonnes = nbPersonnes;
         this.nomClient = nomClient;
@@ -21,5 +22,52 @@ public abstract class Reservation {
         this.heure = heure;
     }
 
-    
-}
+    public ArrayList<Integer> getCodeTable() {
+        return codeTable;
+    }
+
+    public int getNbPersonnes() {
+        return nbPersonnes;
+    }
+
+    public float getHeure() {
+        return heure;
+    }
+
+    public Date getJour() {
+        return jour;
+    }
+
+    public String getNomClient() {
+        return nomClient;
+    }
+
+    public int getTel() {
+        return tel;
+    }
+
+    public void setCodeTable(ArrayList<Integer> codeTable) {
+        this.codeTable = codeTable;
+    }
+
+    public void setHeure(float heure) {
+        this.heure = heure;
+    }
+
+    public void setJour(Date jour) {
+        this.jour = jour;
+    }
+
+    public void setNbPersonnes(int nbPersonnes) {
+        this.nbPersonnes = nbPersonnes;
+    }
+
+    public void setNomClient(String nomClient) {
+        this.nomClient = nomClient;
+    }
+
+    public void setTel(int tel) {
+        this.tel = tel;
+    }
+ 
+   }
