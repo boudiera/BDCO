e57ddfb90c214090394a9300aaf6fc473f3e2137
Clément Michel <Clement.Michel@phelma.grenoble-inp.fr>
2016-04-13 21:42:42 +0200

    Factory

--- RestaurantBDCO/src/FactoriesLayer/ConcreteReservationFactory.java
+++ RestaurantBDCO/src/FactoriesLayer/ConcreteReservationFactory.java
@@ -15,17 +15,22 @@ public class ConcreteReservationFactory extends ReservationFactory{
     @Override
     public ArrayList<Reservation> reservations() {
         try {
-            //  Creation  de la  requete
-            Statement stmt = connexion.getConnection().createStatement();
+            String  STMT_1 = "select * from pilots";
+            String STMT_2;
+            //  Creation de la requete
+            PreparedStatement stmt = connexion.getConnection().prepareStatement(STMT_1);
             //  Execution  de la  requete
-            String  STMT = "select * from pilots";
-            //...
-            ResultSet  rset = stmt.executeQuery (STMT);
+            ResultSet  rsetReservation = stmt.executeQuery ();
+            ResultSet rsetTable;
             //  Conversion  du  resultat  en ArrayList <Pilot >
             ArrayList <Reservation> res = new ArrayList<Reservation> ();
-            while (rset.next()) {
-                res.add(new ConcreteReservation(new ArrayList<Integer>(),
-                            rset.getInt(2), rset.getDate));
+            while (rsetReservation.next()) {
+                STMT_2="select CodeTable from Occupe where CodeReservation =";
+                STMT_2+=rsetReservation.getInt("CodeReservation");
+                stmt=connexion.getConnection().prepareStatement(STMT_2);
+                rsetTable = stmt.executeQuery();
+                
+                res.add(new ConcreteReservation(rset.getInt(), rset.getString(2)));
             }
             //  Fermeture
             rset.close();
