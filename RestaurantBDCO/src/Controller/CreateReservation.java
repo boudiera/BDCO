06671d73e07e1249f0f5f152d05eed78c39be858
IagoFelipe Trentin <Iago-Felipe.Trentin@ensimag.grenoble-inp.fr>
2016-04-15 11:04:03 +0200

    merge iago

--- RestaurantBDCO/src/Controller/CreateReservation.java
+++ RestaurantBDCO/src/Controller/CreateReservation.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ControlModele;
+package Controller;
 
 import Modele.Service;
 import java.util.Date;
