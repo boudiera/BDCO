00b1c3c3c76436fb9b3e7552a05ad05a724110ef
Clément Michel <Clement.Michel@phelma.grenoble-inp.fr>
2016-04-18 11:53:43 +0200

    Rien

--- RestaurantBDCO/src/Modele/RequeteFactory.java
+++ RestaurantBDCO/src/Modele/RequeteFactory.java
@@ -17,8 +17,6 @@ public abstract class RequeteFactory{
     // Permet d'avoir tous les articles, d'un menu pour une carte donnée
     public abstract ArrayList<Article> getArticlesCarteMenu(int codeCarte, TypeArticle typeArticle,Menu menu);
     
-    
-    
     // Renvoie la liste des tables voisines
     public abstract ArrayList<Integer[]> tablesVoisines();
 }
